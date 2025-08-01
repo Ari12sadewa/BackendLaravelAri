7d4610e5428178e39f205318f833fe85c0aebb14
Ari12sadewa <43287768+Ari12sadewa@users.noreply.github.com>
2025-08-01 07:00:34 +0700

    Update cors.php

--- cors.php
+++ cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://frontend-khaki-chi-95.vercel.app'],
+    'allowed_origins' => ['https://front-end-react-indol.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
